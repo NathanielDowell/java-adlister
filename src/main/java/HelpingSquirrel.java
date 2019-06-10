8d2fedcfa65b2069b1b0bfb37a60b34b53432b72
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-06-10 16:19:54 -0500

    feat completed servlets exercise

--- src/main/java/HelpingSquirrel.java
+++ src/main/java/HelpingSquirrel.java
@@ -5,12 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+
+@WebServlet(urlPatterns = "/squirrel")
+public class HelpingSquirrel extends HttpServlet {
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
         try {
             PrintWriter out = res.getWriter();
-            out.println("Hello World!");
+            out.println("Squirrel helps!");
         } catch (IOException e) {
             e.printStackTrace();
         }
